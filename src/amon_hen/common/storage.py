a638d92ce214a545dc5a3c78a05cd7b6e1fdc095
Carson Koball <carson.koball@trojans.dsu.edu>
2026-07-18 23:42:35 -0500

    Fix formatting with black

--- src/amon_hen/common/storage.py
+++ src/amon_hen/common/storage.py
@@ -8,19 +8,21 @@ APP_NAME = "amon-hen"
 
 DATA_DIR = Path(user_data_dir(APP_NAME))
 
+
 def get_data_dir() -> Path:
     """
     Returns the root data directory.
     """
     ensure_dir(DATA_DIR)
-    
+
     return DATA_DIR
-    
-def get_tool_data_dir(tool_name: str) -> Path:
+
+
+def get_script_data_dir(tool_name: str) -> Path:
     """
-    Returns the data directory for a specific tool.
+    Returns the data directory for a specific script.
     """
     path = get_data_dir() / tool_name
     ensure_dir(path)
-    
+
     return path
