7c557839c27e796771e6757eddc1e8729ba4ceb3
Carson Koball <carson.koball@trojans.dsu.edu>
2026-08-10 19:16:53 -0500

    Update UI to integrate new adp_tracker logic

--- ui/handlers/__init__.py
+++ ui/handlers/__init__.py
@@ -1,11 +1,11 @@
 from . import adp_tracker
 from . import blue_list_tracker
-from . import dow_scraper
+from . import dow_parser
 from . import file_tracker
 
 HANDLERS = {
-    "dow_scraper": dow_scraper,
-    "blue_list_tracker": blue_list_tracker,
     "adp_tracker": adp_tracker,
+    "blue_list_tracker": blue_list_tracker,
+    "dow_parser": dow_parser,
     "file_tracker": file_tracker,
 }
--- ui/handlers/adp_tracker.py
+++ ui/handlers/adp_tracker.py
@@ -1,11 +1,12 @@
 from flask import render_template, request
 
-from amon_hen.scripts import adp_tracker
+from amon_hen.tools import adp_tracker
 
 
 def handle(script):
-    new_jobs = None
-    removed_jobs = None
+    cid = ""
+    ccid = ""
+    results = None
 
     if request.method == "POST":
         cid = request.form["cid"]
@@ -13,14 +14,12 @@ def handle(script):
 
         results = adp_tracker.run(cid=cid, ccid=ccid)
 
-        new_jobs = results["new_jobs"]
-        removed_jobs = results["removed_jobs"]
-
     return render_template(
         "adp_tracker.html",
         title=script["name"],
         description=script["description"],
         back_link_visibility="visible",
-        new_jobs=new_jobs,
-        removed_jobs=removed_jobs,
+        cid=cid,
+        ccid=ccid,
+        results=results,
     )
