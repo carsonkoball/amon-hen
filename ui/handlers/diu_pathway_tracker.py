463e48cd82315fdab63d05cd85dfad028f9f7869
Carson Koball <carson.koball@trojans.dsu.edu>
2026-08-31 01:08:17 -0500

    Update diu_pathway_tracker to use new Tracker system

--- ui/handlers/diu_pathway_tracker.py
+++ ui/handlers/diu_pathway_tracker.py
@@ -7,15 +7,15 @@ from amon_hen.tools import diu_pathway_tracker
 
 
 def handle(script):
-    results = None
+    cso_results, ccao_results = None, None
 
     if request.method == "POST":
-        results = diu_pathway_tracker.run()
+        cso_results, ccao_results = diu_pathway_tracker.run()
 
     return render_template(
         "diu_pathway_tracker.html",
         title=script["name"],
         description=script["description"],
         back_link_visibility="visible",
-        results=results,
+        cso_results=cso_results, ccao_results=ccao_results
     )
